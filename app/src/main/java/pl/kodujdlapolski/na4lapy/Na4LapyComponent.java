26e6f2fe5787e1e52ebeb20c1b2376142e25e799
Marek Wojtuszkiewicz <mwojt@users.noreply.github.com>
2016-03-01 19:13:24 +0100

    Usunięto implementację FakeAnimalRepository

--- app/src/main/java/pl/kodujdlapolski/na4lapy/Na4LapyComponent.java
+++ app/src/main/java/pl/kodujdlapolski/na4lapy/Na4LapyComponent.java
@@ -6,9 +6,10 @@ import dagger.Component;
 import pl.kodujdlapolski.na4lapy.activities.MainActivity;
 import pl.kodujdlapolski.na4lapy.api.ApiModule;
 import pl.kodujdlapolski.na4lapy.repository.RepositoryModule;
+import pl.kodujdlapolski.na4lapy.repository.database.DatabaseModule;
 
 @Singleton
-@Component(modules = {Na4LapyModule.class, RepositoryModule.class, ApiModule.class})
+@Component(modules = {Na4LapyModule.class, RepositoryModule.class, ApiModule.class, DatabaseModule.class})
 public interface Na4LapyComponent {
     void inject(MainActivity activity);
 }
