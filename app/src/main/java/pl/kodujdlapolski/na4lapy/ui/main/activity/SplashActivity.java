6d983f55a1099d084bbd62635312b8da74417284
Marek Wojtuszkiewicz <mwojt@users.noreply.github.com>
2016-03-04 01:45:15 +0100

    Przeniesienie i zmiana nazwy MainActivity -> SplashActivity

---
+++ app/src/main/java/pl/kodujdlapolski/na4lapy/ui/main/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package pl.kodujdlapolski.na4lapy.ui.main.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import pl.kodujdlapolski.na4lapy.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
