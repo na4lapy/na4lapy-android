b1ffbc6f77a119261882a1fde92edb737ce0cc6d
Marek Wojtuszkiewicz <mwojt@users.noreply.github.com>
2016-03-07 23:22:52 +0100

    Dodanie biblioteki Retrofit i wstępna konfiguracja

--- app/src/main/java/pl/kodujdlapolski/na4lapy/api/ApiServiceImpl.java
+++ app/src/main/java/pl/kodujdlapolski/na4lapy/api/ApiServiceImpl.java
@@ -1,11 +1,25 @@
 package pl.kodujdlapolski.na4lapy.api;
 
+import android.util.Log;
+
+import java.io.IOException;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import pl.kodujdlapolski.na4lapy.model.Animal;
 import pl.kodujdlapolski.na4lapy.model.Shelter;
+import retrofit2.Call;
 
 public class ApiServiceImpl implements ApiService {
+
+    private Api mApi;
+
+    @Inject
+    public ApiServiceImpl(Api api) {
+        mApi = api;
+    }
+
     @Override
     public List<Animal> getAllAnimals() {
         //TODO
@@ -14,7 +28,13 @@ public class ApiServiceImpl implements ApiService {
 
     @Override
     public List<Shelter> getAllShelters() {
-        //TODO
-        return null;
+        Call<List<Shelter>> sheltersCall = mApi.listShelters();
+        List<Shelter> shelters = null;
+        try {
+            shelters = sheltersCall.execute().body();
+        } catch (IOException e) {
+            Log.w(getClass().getSimpleName(), e);
+        }
+        return shelters;
     }
 }
--- app/src/prod/java/pl/kodujdlapolski/na4lapy/api/ApiModule.java
+++ app/src/prod/java/pl/kodujdlapolski/na4lapy/api/ApiModule.java
@@ -4,13 +4,38 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.HttpUrl;
+import pl.kodujdlapolski.na4lapy.BuildConfig;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class ApiModule {
 
     @Singleton
     @Provides
-    public ApiService provideApiService() {
-        return new ApiServiceImpl();
+    public ApiService provideApiService(Api api) {
+        return new ApiServiceImpl(api);
+    }
+
+    @Singleton
+    @Provides
+    public HttpUrl provideBaseUrl() {
+        return HttpUrl.parse(BuildConfig.BASE_URL);
+    }
+
+    @Singleton
+    @Provides
+    public Retrofit provideRetrofit(HttpUrl baseUrl) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    public Api provideApi(Retrofit retrofit) {
+        return retrofit.create(Api.class);
     }
 }
