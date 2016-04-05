b9c01b2e6e357b3fd08c919c4689e0abe18da0b5
Gosia Syska <malgorzata.syska@o2.pl>
2016-04-06 00:52:35 +0200

    Testy do drawer activity, refactor

--- app/src/androidTest/java/pl/kodujdlapolski/na4lapy/DrawerActivityTest.java
+++ app/src/androidTest/java/pl/kodujdlapolski/na4lapy/DrawerActivityTest.java
@@ -1,22 +1,32 @@
 package pl.kodujdlapolski.na4lapy;
 
 import android.content.Intent;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import pl.kodujdlapolski.na4lapy.ui.about_shelter.AboutShelterActivity;
+import pl.kodujdlapolski.na4lapy.ui.animals_list.AnimalsBrowseActivity;
+import pl.kodujdlapolski.na4lapy.ui.animals_list.AnimalsFavListActivity;
 import pl.kodujdlapolski.na4lapy.ui.main.activity.SplashActivity;
+import pl.kodujdlapolski.na4lapy.ui.preferences.PreferencesActivity;
+import pl.kodujdlapolski.na4lapy.ui.settings.SettingsActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 
 /**
  * Created by Gosia on 2016-03-27.
@@ -29,6 +39,7 @@ public class DrawerActivityTest {
     @Before
     public void openDrawerMenu() {
         activityRule.launchActivity(new Intent());
+        onView(withText(R.string.browsing)).perform(click());
         onView(withContentDescription(activityRule.getActivity().getString(R.string.navigation_drawer_open)))
                 .perform(click());
         onView(withId(R.id.nav_view)).check(matches(isDisplayed()));
@@ -52,40 +63,68 @@ public class DrawerActivityTest {
 
     @Test
     public void doesClickOnBrowseDirectsToBrowsingActivity() {
-        onView(withText(R.string.browsing)).perform(click());
-        onView(withText(R.string.animals_list_activity)).check(matches(isDisplayed()));
+        onView(withText(R.string.lookupPreferences)).perform(click());
+        onView(withContentDescription(activityRule.getActivity().getString(R.string.navigation_drawer_open)))
+                .perform(click());
+
+        isProperActivityDisplayed(
+                R.string.browsing,
+                AnimalsBrowseActivity.class,
+                AnimalsBrowseActivity.EXTRA_IS_SINGLE_ELEMENT_BROWSE,
+                true);
     }
 
     @Test
     public void doesClickOnFavouritesDirectsToFavouritesListActivity() {
-        onView(withText(R.string.favouritiesList)).perform(click());
-        onView(withText(R.string.fav_animals_list_activity)).check(matches(isDisplayed()));
+        isProperActivityDisplayed(
+                R.string.favouritiesList,
+                AnimalsFavListActivity.class,
+                AnimalsFavListActivity.EXTRA_IS_FAV_LIST,
+                true);
     }
 
     @Test
     public void doesClickOnPreferencesDirectsToPreferencesActivity() {
-        onView(withText(R.string.lookupPreferences)).perform(click());
-        onView(withId(R.id.preferences_layout)).check(matches(isDisplayed()));
+        isProperActivityDisplayed(
+                R.string.lookupPreferences,
+                PreferencesActivity.class);
     }
 
     @Test
     public void doesClickAboutShelterDirectsToAboutShelterActivity() {
-        onView(withText(R.string.aboutShelter)).perform(click());
-        onView(withId(R.id.about_shelter_app_fragment)).check(matches(isDisplayed()));
+        isProperActivityDisplayed(
+                R.string.aboutShelter,
+                AboutShelterActivity.class);
     }
 
     @Test
     public void doesClickOnSettingsDirectsToSettingsActivity() {
-        onView(withText(R.string.accountSettings)).perform(click());
-        onView(withId(R.id.about_app_fragment)).check(matches(isDisplayed()));
+        isProperActivityDisplayed(
+                R.string.accountSettings,
+                SettingsActivity.class);
     }
 
-    //todo - trzeba zauktualizowac kod - od Natalii
     @Test
     public void isNavigationItemSelectedWhenYouClickIt() {
         onView(withText(R.string.browsing)).perform(click());
         onView(withContentDescription(activityRule.getActivity().getString(R.string.navigation_drawer_open)))
                 .perform(click());
         onView(withText(R.string.browsing)).check(matches(isChecked()));
+    }
+
+    private void isProperActivityDisplayed (int category, Class<?> clazz, String extraKey, boolean extraValue) {
+        Intents.init();
+        onView(withText(category)).perform(click());
+        intended(
+            hasComponent(hasClassName(clazz.getName())));
+        if (extraValue) {
+            intended(
+                hasExtra(extraKey, extraValue));
+        }
+        Intents.release();
+    }
+
+    private void isProperActivityDisplayed(int category, Class<?> clazz) {
+        isProperActivityDisplayed(category, clazz, "", false);
     }
 }
