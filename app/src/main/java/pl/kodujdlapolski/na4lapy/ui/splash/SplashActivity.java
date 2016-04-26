1d28e204c59be499a58d873e36b6c6edba099e8a
Natalia Wroblewska <s7313@pjwstk.edu.pl>
2016-04-26 22:02:26 +0200

    dodałam (na razie zakomentowaną) obslugę pokazywania wprowadzenia tylko raz.

--- app/src/main/java/pl/kodujdlapolski/na4lapy/ui/splash/SplashActivity.java
+++ app/src/main/java/pl/kodujdlapolski/na4lapy/ui/splash/SplashActivity.java
@@ -7,13 +7,19 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import javax.inject.Inject;
+
 import pl.kodujdlapolski.na4lapy.Na4LapyApp;
 import pl.kodujdlapolski.na4lapy.R;
+import pl.kodujdlapolski.na4lapy.preferences.PreferencesModule;
+import pl.kodujdlapolski.na4lapy.preferences.PreferencesService;
 import pl.kodujdlapolski.na4lapy.ui.browse.AbstractBrowseActivity;
 import pl.kodujdlapolski.na4lapy.ui.browse.single.SingleBrowseActivity;
 import pl.kodujdlapolski.na4lapy.ui.introduction.IntroductionActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    @Inject
+    PreferencesService preferencesService;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,25 +27,19 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ((Na4LapyApp) getApplication()).getComponent().inject(this);
-
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                findViewById(R.id.goToBrowse).performClick();
-            }
-        }, 100);
-
+        goToBrowse();
     }
 
-    public void goToBrowse(View view) {
+    public void goToBrowse() {
         TaskStackBuilder b = TaskStackBuilder.create(this);
         Intent browse = new Intent(SplashActivity.this, SingleBrowseActivity.class);
         browse.putExtra(AbstractBrowseActivity.EXTRA_IS_FAV_LIST, false);
         browse.putExtra(AbstractBrowseActivity.EXTRA_IS_SINGLE_ELEMENT_BROWSE, true);
-        Intent intent1 = new Intent(this, IntroductionActivity.class);
         b.addNextIntent(browse);
+        //    if (preferencesService.shouldIntroductionBeShown()) { // todo remove comment when introduction is done
+        Intent intent1 = new Intent(this, IntroductionActivity.class);
         b.addNextIntent(intent1);
+        // }
         b.startActivities();
         finish();
     }
