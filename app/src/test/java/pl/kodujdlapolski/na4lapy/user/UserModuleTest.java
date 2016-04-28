71b85e02e15b4ade8b0113daca1d2e154d8312fa
Marek Wojtuszkiewicz <mwojt@users.noreply.github.com>
2016-04-28 21:44:39 +0200

    Testy jednostkowe dla UserService

---
+++ app/src/test/java/pl/kodujdlapolski/na4lapy/user/UserModuleTest.java
@@ -0,0 +1,34 @@
+package pl.kodujdlapolski.na4lapy.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import pl.kodujdlapolski.na4lapy.preferences.PreferencesService;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserModuleTest {
+
+    @Mock
+    private PreferencesService preferencesService;
+
+    private UserModule userModule;
+
+    @Before
+    public void setUp() throws Exception {
+        userModule = new UserModule();
+    }
+
+    @Test
+    public void testProvideRepositoryService() throws Exception {
+        // when
+        UserService result = userModule.provideUserService(preferencesService);
+
+        // then
+        assertNotNull(result);
+    }
+}
