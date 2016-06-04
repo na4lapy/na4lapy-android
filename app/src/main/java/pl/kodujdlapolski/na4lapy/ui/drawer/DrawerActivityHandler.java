f49d83e41b83ee03cb2fc6ab283fc12eac0ba79c
Gosia Syska <malgorzata.syska@o2.pl>
2016-06-04 15:53:51 +0200

    Licencja apache w UI: drawer-menu i preferencje

--- app/src/main/java/pl/kodujdlapolski/na4lapy/ui/drawer/DrawerActivityHandler.java
+++ app/src/main/java/pl/kodujdlapolski/na4lapy/ui/drawer/DrawerActivityHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2016 [name of copyright owner]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package pl.kodujdlapolski.na4lapy.ui.drawer;
 
 import android.content.Context;
@@ -28,9 +43,6 @@ import pl.kodujdlapolski.na4lapy.ui.preferences.PreferencesActivity;
 import pl.kodujdlapolski.na4lapy.ui.settings.SettingsActivity;
 import pl.kodujdlapolski.na4lapy.user.UserService;
 
-/**
- * Created by Malgorzata Syska on 2016-03-14.
- */
 public class DrawerActivityHandler {
 
     @Inject
