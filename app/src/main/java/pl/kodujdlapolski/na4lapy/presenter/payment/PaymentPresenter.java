234c2c69f97ad64acb1766dad4ba0cdf1c3ab01b
Natalia Wróblewska <natalia.wroblewska@oke.pl>
2016-12-27 20:34:50 +0100

    licencja wszystie pliki java

--- app/src/main/java/pl/kodujdlapolski/na4lapy/presenter/payment/PaymentPresenter.java
+++ app/src/main/java/pl/kodujdlapolski/na4lapy/presenter/payment/PaymentPresenter.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (C) 2016 Stowarzyszenie Na4Łapy
+ *	Copyright 2017 Stowarzyszenie Na4Łapy
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *	http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
  */
 
 package pl.kodujdlapolski.na4lapy.presenter.payment;
@@ -45,9 +45,6 @@ import pl.kodujdlapolski.na4lapy.utils.formvalidator.ValidationError;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-/**
- * @author Marek Wojtuszkiewicz
- */
 public class PaymentPresenter implements PaymentContract.UserActionListener {
 
     @Inject
